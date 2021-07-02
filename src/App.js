b0480065a1571bfe9be074d7a7ec8c6773019f2d
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-07-02 16:06:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 function App() {
+  
+  const[toggle, setToggle] = useState(false);
+  const[allFoods, setAllFoods] = useState(foods);
+   //console.log(toggle)
+  const handleSubmit = () => {
+    
+  }
+
+  const handleChange = () => {
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FoodBox food = {foods}/>
+      {toggle &&
+      <form onSubmit = {handleSubmit} >
+        <input onChange = {handleChange} type="text" name="name" placeholder="Food name"/>
+        <input onChange = {handleChange} type="number" name="calories" placeholder="Calories count"/>
+        <input onChange = {handleChange} type="text" name="image" placeholder="Image"/>
+        <button>Submit</button>
+      </form>
+      }
+     
+      <button onClick = {()=> setToggle(!toggle)}>Add new food</button>
     </div>
   );
 }
---
+++ src/components/FoodBox.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+function FoodBox({food}) {
+    const displayFood = () => {
+        return food.map((element) => {
+            return (
+            
+<div className="box">
+    <article className="media">
+        <div className="media-left">
+            <figure className="image is-64x64">
+                <img src={element.image}/>
+            </figure>
+        </div>
+        <div className="media-content">
+            <div className="content">
+                <p>
+                    <strong>{element.name}</strong>
+                    <br/>
+                    <small>{element.calories} cal</small>
+                </p>
+            </div>
+        </div>
+        <div className="media-right">
+            <div className="field has-addons">
+                <div className="control">
+                    <input className="input" type="number" value="1"/>
+                </div>
+                <div className="control">
+                    <button className="button is-info">
+                        +
+                    </button>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
+                
+            )
+        })
+    }
+    console.log(food)
+    return (
+     <div>
+         {displayFood()}
+     </div>
+        
+       
+    )
+}
+
+export default FoodBox
